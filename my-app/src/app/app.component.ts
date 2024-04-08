469915a9dac58865673c95c81fe68abb62d138a9
DOMANA19 <166106613+DOMANA19@users.noreply.github.com>
2024-04-08 08:40:35 +0000

    timer

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar/calendar.component';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CalendarComponent],
+  imports: [CalendarComponent, TimerComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
